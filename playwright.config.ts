d3eebb0efd29b52863c32e0efbb9259f2965a84b
Kamran Khan <kamrankhan54@hotmail.com>
2025-10-29 11:09:36 +0000

    config updates to allow playwright find the cucumber tests to run

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from '@playwright/test';
 import { defineBddConfig } from 'playwright-bdd';
 
 const testDir = defineBddConfig({
-  features: 'tests/features/*.feature',
-  steps: 'tests/steps/*.js',
+  features: 'tests/features/**/*.feature',
+  steps: 'tests/steps/**/*.ts',
 });
 
 export default defineConfig({
